3b7a56f9cd38019b64c0ee5e18a71959d46c6e1e
Muhammed Yusa Sarisoy <yusasarisoy@icloud.com>
2025-08-08 16:52:51 +0300

    Initial Commit

---
+++ NextWander/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextWander
+//
+//  Created by Yusa Sarisoy on 08.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextWander/NextWanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextWanderApp.swift
+//  NextWander
+//
+//  Created by Yusa Sarisoy on 08.08.25.
+//
+
+import SwiftUI
+
+@main
+struct NextWanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
